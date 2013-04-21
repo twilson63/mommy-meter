98ffad8dd22981153ab2955498d2ce03aff567a8
Tom Wilson <tom@jackhq.com>
2013-04-20 22:08:01 -0400

    added interceptor

--- app.js
+++ app.js
@@ -2,39 +2,100 @@ var express = require('express');
 var app = express();
 var nano = require('nano')('http://localhost:5984');
 
-nano.db.create('mm_guest_development');
-var userdb = nano.use('mm_guest_development');
+var usersdb = nano.use('_users');
+var appdb = nano.use('mm_guest_development');
+
+var modelDoc = {
+  language: "javascript",
+  views: {
+    all: {
+      map: "function(doc) {\n  emit(doc.type, doc);\n}"
+    },
+    get: {
+      map: "function(doc) {\n  if (/_/.test(doc.type)) {\n    emit([\n      doc.type, \n      doc[doc.type].split('_')[0]\n    ], doc);\n  } else {\n    emit([\n      doc.type,\n      doc._id\n    ], doc);\n  }\n}"
+    }
+  }
+};
 
 app.configure(function() {
   app.use(express.bodyParser());
-  //app.use(express.router);
+  //app.use(app.router);
+  app.use(express.cookieParser('shhhh, very secret'));
+  app.use(express.session());
   app.use(express.static('./public'));
 });
 
-app.post('/api/:model', function(req, res) {
+app.post('/api/signup', function(req, res) {
+  var username = req.body.name;
+  req.body.roles = [username];
+  req.body.type = 'user';
+  usersdb.insert(req.body, 'org.couchdb.user:' + username, function(err) {
+    if (err) { return res.send(500, err); }
+    nano.db.create('mm_' + username, function(err) {
+      if (err) { return res.send(500, err); }
+      var doc = { admins: { name: [], roles: []}, members: { names: [username], roles: []}};
+      //appdb = nano.use('mm_' +username);
+      // insert design document
+      appdb.insert(modelDoc, '_design/model');
+      appdb.insert(doc, '_security').pipe(res);
+    });
+  });
+});
+
+app.post('/api/login', function(req, res) {
+  // authorize and set token or return 401
+  //console.log(req.body);
+  nano.auth(req.body.name, req.body.password, function(err, body, headers) {
+    if (err) { return res.send(500, err); }
+    req.session.user = body.name;
+    req.session.appdb = 'mm_' + body.name;
+    req.session[body.name] = headers['set-cookie'];
+    // set database...
+    res.send(200);
+  });
+});
+
+app.post('/api/logout', function(req, res) {
+  delete res.session[req.session.user];
+  delete req.session.user;
+  delete req.session.appdb;
+  res.send(200);
+});
+
+app.post('/api/:model', restrict(), function(req, res) {
   req.body.type = req.params.model;
-  userdb.insert(req.body).pipe(res);
+  req.db.insert(req.body).pipe(res);
 });
 
-app.get('/api/:model', function(req, res) {
-  //userdb.list.pipe(res);
-  userdb.view('model', 'all', { key: req.params.model }).pipe(res);
+app.get('/api/:model', restrict(), function(req, res) {
+  req.db.view('model', 'all', { key: req.params.model }).pipe(res);
 });
-app.get('/api/:model/:id', function(req, res) {
-  userdb.view('model', 'get', { key: [req.params.model, req.params.id] }).pipe(res);
-});
-
-app.put('/api/:model/:id', function(req, res) {
-  userdb.insert(req.body, req.params.id).pipe(res);
+app.get('/api/:model/:id', restrict(), function(req, res) {
+  req.db.view('model', 'get', { key: [req.params.model, req.params.id], include_docs: true }).pipe(res);
 });
 
-app.del('/api/:model/:id', function(req, res) {
-  userdb.destroy(req.params.id, req.query.rev).pipe(res);
+app.put('/api/:model/:id', restrict(), function(req, res) {
+  req.db.insert(req.body, req.params.id).pipe(res);
+});
+
+app.del('/api/:model/:id', restrict(), function(req, res) {
+  req.db.destroy(req.params.id, req.query.rev).pipe(res);
 });
 
 app.get(/(?!\.)/, function(req, res) {
   res.sendfile('./public/index.html');
 });
 
+function restrict() {
+  return function(req, res, next) {
+    console.log(req.session);
+    if (req.session.user) {
+      req.db = nano.use(req.session.appdb);
+      next();
+    } else {
+      res.send(401, { message: 'Unauthorized: must be logged in to request this funciton!'});
+    }
+  };
+}
 
 app.listen(3000);
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,35 +1,68 @@
-var app = angular.module('App', ['ui.bootstrap'])
+var app = angular.module('App', ['ui.bootstrap','http-auth-interceptor'])
   .config(function($routeProvider, $locationProvider){
     $routeProvider
       .when('/', { controller: 'IndexCtrl', templateUrl: '/assets/templates/index.html'})
       .when('/signup', {controller: 'SignUpCtrl', templateUrl: '/assets/templates/profile/form.html'})
+      .when('/login', {controller: 'LoginCtrl', templateUrl: '/assets/templates/login.html'})
       .when('/profile', {controller: 'ProfileCtrl', templateUrl: '/assets/templates/profile/form.html'})
       .when('/tasks', {controller: 'TasksCtrl', templateUrl: '/assets/templates/tasks/index.html'})
       .when('/tasks/new', {controller: 'TaskNewCtrl', templateUrl: '/assets/templates/tasks/form.html'})
       .when('/tasks/:id', {controller: 'TaskShowCtrl', templateUrl: '/assets/templates/tasks/show.html'})
       .when('/tasks/:id/edit', {controller: 'TaskEditCtrl', templateUrl: '/assets/templates/tasks/form.html'})
-      .when('/records', {controller: 'TasksCtrl', templateUrl: '/assets/templates/records/index.html'})
-      .when('/records/new', {controller: 'TaskNewCtrl', templateUrl: '/assets/templates/records/form.html'})
-      .when('/records/:id', {controller: 'TaskShowCtrl', templateUrl: '/assets/templates/records/show.html'})
-      .when('/records/:id/edit', {controller: 'TaskEditCtrl', templateUrl: '/assets/templates/records/form.html'})
+      .when('/records', {controller: 'RecordsCtrl', templateUrl: '/assets/templates/records/index.html'})
+      .when('/records/new', {controller: 'RecordNewCtrl', templateUrl: '/assets/templates/records/form.html'})
+      .when('/records/:id', {controller: 'RecordShowCtrl', templateUrl: '/assets/templates/records/show.html'})
+      .when('/records/:id/edit', {controller: 'RecordEditCtrl', templateUrl: '/assets/templates/records/form.html'})
       ;
     $locationProvider.html5Mode(true);
   })
-  .controller('IndexCtrl', function($scope) {
-    
+  .run(function($rootScope, $location) {
+    $rootScope.$on('event:auth-loginRequired', function() {
+      $location.path('/login');
+    });
+    $rootScope.$on('event:auth-loginConfirmed', function() {
+      $location.path('/');
+    });
   })
-  .controller('TasksCtrl', function($scope, $http, $_) {
-    $http.get('/api/tasks').success(function(data) {
-      console.log(data);
+  .controller('IndexCtrl', function($scope, $http) {
+    $scope.stats = {};
+    $http.get('/api/record').success(function(data) {
+      $scope.stats.records = data.rows.length;
+    });
+  })
+  .controller('LoginCtrl', function($scope, $http, $location, authService) {
+    $scope.login = function(user) {
+      $http.post('/api/login', user)
+        .success(function(results) {
+          authService.loginConfirmed();
+        });
+    }
+  })
+  .controller('SignUpCtrl', function($scope, $http, $location) {
+    $scope.save = function(profile) {
+      $http.post('/api/signup', profile)
+        .success(function(results) {
+          console.log(results);
+          $location.path('/');
+        });
+    };
+    $scope.cancel = function() {
+      $location.path('/login');
+    };
+  })
+  .controller('TasksCtrl', function($scope, $http, $_, $location) {
+    $http.get('/api/task').success(function(data) {
       $scope.tasks = $_(data.rows).pluck('value');
     });
-    console.log('tasks');
+    $scope.edit = function(task) {
+      $location.path('/tasks/' + task._id + '/edit');
+    };
   })
   .controller('TaskNewCtrl', function($scope, $location, $http) {
     $scope.mode = "New";
     $scope.save = function(task) {
       // save task
-      $http.post('/api/tasks', task)
+      $http.post('/api/task', task)
         .success(function(data) {
           $location.path('/tasks');
           // add alert
@@ -40,14 +73,16 @@ var app = angular.module('App', ['ui.bootstrap'])
     };
     $scope.cancel = function() { $location.path('/tasks'); }
   })
-  .controller('TaskEditCtrl', function($scope) {
+  .controller('TaskEditCtrl', function($scope, $http, $routeParams, $location) {
     $scope.mode = "Edit";
-    $scope.task = $get('/api/tasks/' + task._id);
+    $http.get('/api/task/' + $routeParams.id).success(function(data) {
+      $scope.task = data.rows[0].value;
+    });
     $scope.save = function(task) {
       // save task
-      $http.put('/api/tasks/' + task._id)
+      $http.put('/api/task/' + task._id, task)
         .success(function(data) {
-          $location.path('/tasks/' + task._id);
+          $location.path('/tasks');
           // add alert
         })
         .error(function(err) {
@@ -60,8 +95,29 @@ var app = angular.module('App', ['ui.bootstrap'])
   .controller('TaskShowCtrl', function($scope) {
     
   })
-  .controller('RecordNewCtrl', function($scope) {
+  .controller('RecordNewCtrl', function($scope, $routeParams, $http, $location) {
     $scope.mode = "New";
+    $scope.record = { task: $routeParams.task };
+    $http.get('/api/task/' + $scope.record.task)
+      .success(function(results) {
+        var task = results.rows[0].value;
+        $scope.record.name = task.name;
+        $scope.record.taskType = task.taskType
+        $scope.record.value = task.value;
+      });
+    $scope.save = function(record) {
+      // save record
+      record.task = $routeParams.task;
+      $http.post('/api/record', record)
+        .success(function(data) {
+          $location.path('/tasks');
+          // add alert
+        })
+        .error(function(err) {
+          // add alert
+        });
+    };
+    $scope.cancel = function() { $location.path('/tasks'); }
   })
   .controller('RecordEditCtrl', function($scope) {
     $scope.mode = "Edit";
